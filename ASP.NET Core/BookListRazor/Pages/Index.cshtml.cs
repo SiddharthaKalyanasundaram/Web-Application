22ac6f9a384299577f17631eabbc1f5a0a487fbd
siddhartha1988 <siddeee@gmail.com>
2020-12-13 20:44:20 +0530

    ASP.NET Core Basics

---
+++ ASP.NET Core/BookListRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IList<Employee> Employees { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Employees = new List<Employee>() { new Employee { Id = "1", Name = "Siddy" }, new Employee { Id = "2", Name = "Durga" } };
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ ASP.NET Core/HTMLTagHelpers/Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace HTMLTagHelpers.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IList<Employee> Employees { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Employees = new List<Employee>() { new Employee { Id = "1", Name = "Siddy" }, new Employee { Id = "2", Name = "Durga" } };
+            _logger = logger;
+        }
+        
+        public void OnGetDetails(int id)
+        {
+            // return new LocalRedirectResult(Request.Path);
+        }
+
+            public void Details(int id)
+        {
+            // return new LocalRedirectResult(Request.Path);
+        }
+
+        public void OnDetails(int id)
+        {
+            // return new LocalRedirectResult(Request.Path);
+        }
+
+        public void OnGet(int id)
+        {
+            //return View();
+        }
+
+        public void OnPost(int id)
+        {
+
+        }
+    }
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
